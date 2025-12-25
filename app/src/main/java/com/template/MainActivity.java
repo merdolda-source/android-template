4fa7acd9c8476e1daae6bc96ca93d9f59b1163ca
merdolda-source <merdolda@gmail.com>
2025-12-26 00:27:19 +0300

    Update MainActivity.java

--- app/src/main/java/com/template/MainActivity.java
+++ app/src/main/java/com/template/MainActivity.java
@@ -1,16 +1,40 @@
 package com.template;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle b) {
-        super.onCreate(b);
-        WebView w = new WebView(this);
-        w.getSettings().setJavaScriptEnabled(true);
-        w.loadUrl("https://example.com");
-        setContentView(w);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // 🔴 BURAYI PHP'DEN OTOMATİK DEĞİŞTİRECEKSİN
+        webView.loadUrl("https://example.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content);
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
